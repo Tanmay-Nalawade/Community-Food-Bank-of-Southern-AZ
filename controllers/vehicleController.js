fa228ed499f9d0cd893d50a7ac16b22aef47fa98
Tanmay Nalawade <tanmaynalawade1@gmail.com>
2026-06-10 22:18:21 -0700

    add vehicle route

--- controllers/vehicleController.js
+++ controllers/vehicleController.js
@@ -1,5 +1,25 @@
 const Vehicle = require("../models/vehicle");
 
+exports.getAddVehicle = (req, res) => {
+  res.render("vehicles/add", {
+    title: "Add Vehicle",
+  });
+}
+
+exports.postAddVehicle = async (req, res) => {
+  const { make, model, year, licensePlate, keyCafeKeyId, currentMileage } = req.body;
+  const newVehicle = new Vehicle({
+    make,
+    model,
+    year,
+    licensePlate,
+    keyCafeKeyId,
+    currentMileage,
+  });
+  await newVehicle.save();
+  res.redirect("/vehicles");
+};
+
 exports.index = async (req, res) => {
   const vehicles = await Vehicle.find({}).sort({ make: 1, model: 1 });
   res.render("vehicles/index", {
--- routes/vehicleRoutes.js
+++ routes/vehicleRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const vehicles = require("../controllers/vehicleController");
+const vehiclesController = require("../controllers/vehicleController");
 
-router.get("/", vehicles.index);
+router.get("/", vehiclesController.index);
+
+router.get("/add", vehiclesController.getAddVehicle);
 
 module.exports = router;
