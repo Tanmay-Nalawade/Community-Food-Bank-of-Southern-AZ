9fec94b48f06b4f061b93125f073c4da42581775
Tanmay Nalawade <tanmaynalawade1@gmail.com>
2026-06-11 22:09:26 -0700

    add Vehicle complete

--- routes/vehicleRoutes.js
+++ routes/vehicleRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const vehiclesController = require("../controllers/vehicleController");
 
 router.get("/", vehiclesController.index);
-
 router.get("/add", vehiclesController.getAddVehicle);
+router.post("/add", vehiclesController.postAddVehicle);
 
 module.exports = router;
