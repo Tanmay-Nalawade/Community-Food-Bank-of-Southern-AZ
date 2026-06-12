bb438a0f80fa35abeb2827275e766f6299746471
Tanmay Nalawade <tanmaynalawade1@gmail.com>
2026-06-11 22:14:54 -0700

    detail view of a vehicle

--- controllers/vehicleController.js
+++ controllers/vehicleController.js
@@ -1,11 +1,23 @@
 const Vehicle = require("../models/vehicle");
 
+// To display the list of all vehicles
+exports.index = async (req, res) => {
+  const vehicles = await Vehicle.find({}).sort({ make: 1, model: 1 });
+  res.render("vehicles/index", {
+    title: "Vehicles",
+    vehicles,
+  });
+};
+
+
+// To get the form for adding a new vehicle
 exports.getAddVehicle = (req, res) => {
   res.render("vehicles/add", {
     title: "Add Vehicle",
   });
 }
 
+// To handle the submission of the new vehicle form
 exports.postAddVehicle = async (req, res) => {
   const { make, model, year, licensePlate, keyCafeKeyId, currentMileage } = req.body;
   const newVehicle = new Vehicle({
@@ -20,10 +32,16 @@ exports.postAddVehicle = async (req, res) => {
   res.redirect("/vehicles");
 };
 
-exports.index = async (req, res) => {
-  const vehicles = await Vehicle.find({}).sort({ make: 1, model: 1 });
-  res.render("vehicles/index", {
-    title: "Vehicles",
-    vehicles,
+
+// To view details of a specific vehicle
+exports.viewVehicle = async (req, res) => {
+  const vehicleId = req.params.id;
+  const vehicle = await Vehicle.findById(vehicleId);
+  if (!vehicle) {
+    return res.status(404).send("Vehicle not found");
+  }
+  res.render("vehicles/view", {
+    title: `${vehicle.make} ${vehicle.model}`,
+    vehicle,
   });
 };
--- routes/vehicleRoutes.js
+++ routes/vehicleRoutes.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const vehiclesController = require("../controllers/vehicleController");
 
 router.get("/", vehiclesController.index);
+
 router.get("/add", vehiclesController.getAddVehicle);
 router.post("/add", vehiclesController.postAddVehicle);
+
+router.get("/:id", vehiclesController.viewVehicle);
 
 module.exports = router;
