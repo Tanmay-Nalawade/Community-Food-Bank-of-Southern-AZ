092dafbf4b58bb5c8fe00ec1f6d4678541a5f38d
Tanmay Nalawade <tanmaynalawade1@gmail.com>
2026-09-08 21:54:15 -0700

    add cashing on partials and reduce request load

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use(
   }),
 );
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public"), { maxAge: "1h" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(mongoSanitize());
 app.use(methodOverride("_method"));
